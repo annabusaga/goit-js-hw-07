94a1e435add56bb8ac61fec204d4523e607cde49
annabusaga <anna.busaga@gmail.com>
2023-02-09 14:03:51 +0200

    task7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+
+const markup = galleryItems.map(item=> `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join('')
+
+const galleryRef = document.querySelector('.gallery')
+
+
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
+
